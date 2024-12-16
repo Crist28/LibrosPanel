2bd2b1c65dd0475a2da37a188cfdc38429901974
Lufer2801 <luisa230313@gmail.com>
2024-12-16 10:13:37 -0500

    Test Prettier en Angular

--- src/app/pages/books/galeria-imagenes/galeria-imagenes.component.ts
+++ src/app/pages/books/galeria-imagenes/galeria-imagenes.component.ts
@@ -1,7 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule, FormsModule, NgForm, FormGroup, FormControl } from '@angular/forms';
-import { RowComponent, ColComponent, TextColorDirective, CardComponent, CardHeaderComponent, CardBodyComponent, FormDirective, FormLabelDirective, FormControlDirective, FormFeedbackComponent, InputGroupComponent, InputGroupTextDirective, FormSelectDirective, FormCheckComponent, FormCheckInputDirective, FormCheckLabelDirective, ButtonDirective, ListGroupDirective, ListGroupItemDirective } from '@coreui/angular';
+import {
+  ReactiveFormsModule,
+  FormsModule,
+  NgForm,
+  FormGroup,
+  FormControl,
+} from '@angular/forms';
+import {
+  RowComponent,
+  ColComponent,
+  TextColorDirective,
+  CardComponent,
+  CardHeaderComponent,
+  CardBodyComponent,
+  FormDirective,
+  FormLabelDirective,
+  FormControlDirective,
+  FormFeedbackComponent,
+  InputGroupComponent,
+  InputGroupTextDirective,
+  FormSelectDirective,
+  FormCheckComponent,
+  FormCheckInputDirective,
+  FormCheckLabelDirective,
+  ButtonDirective,
+  ListGroupDirective,
+  ListGroupItemDirective,
+} from '@coreui/angular';
 import { DefaultLayoutComponent } from '../../../components/default-layout/default-layout.component';
 import {
   TableDirective,
@@ -10,9 +36,7 @@ import {
   BorderDirective,
   AlignDirective,
 } from '@coreui/angular';
-import {
-  ColDirective,
-} from '@coreui/angular';
+import { ColDirective } from '@coreui/angular';
 import { BookCreate } from '../../../environments/global.component';
 import { AdminService } from '../../../services/admin.service';
 import { Router, RouterLink, ActivatedRoute } from '@angular/router';
@@ -51,21 +75,24 @@ declare let iziToast: any;
     TableActiveDirective,
     BorderDirective,
     AlignDirective,
-    CommonModule
+    CommonModule,
   ],
   templateUrl: './galeria-imagenes.component.html',
-  styleUrl: './galeria-imagenes.component.scss'
+  styleUrl: './galeria-imagenes.component.scss',
 })
 export class GaleriaImagenesComponent {
   public imgSelect: any | ArrayBuffer = 'assets/carpeta.jpg';
   public token: string;
-  public files : any = undefined;
+  public files: any = undefined;
   public id: any;
   public libro: any;
   public url;
   public tooltipValidated = false;
 
-  constructor(private adminService: AdminService, private route: ActivatedRoute,){
+  constructor(
+    private adminService: AdminService,
+    private route: ActivatedRoute,
+  ) {
     const token = this.adminService.getToken();
     this.token = token !== null ? token : '';
 
@@ -76,52 +103,55 @@ export class GaleriaImagenesComponent {
     this.init_data();
   }
 
-  init_data(){
+  init_data() {
     this.route.params.subscribe((params) => {
       this.id = params['id'];
-      this.adminService
-        .getBooks(this.id, this.token)
-        .subscribe(
-          (response) => {
-            if (response.data == undefined) {
-              this.libro = undefined;
-            } else {
-              this.libro = response.data;
-            }
-            console.log(this.libro);
-          },
-          (error) => {
-            console.log(error);
+      this.adminService.getBooks(this.id, this.token).subscribe(
+        (response) => {
+          if (response.data == undefined) {
+            this.libro = undefined;
+          } else {
+            this.libro = response.data;
           }
-        );
+          console.log(this.libro);
+        },
+        (error) => {
+          console.log(error);
+        },
+      );
     });
   }
 
   subir_imagenes() {
     if (this.files && this.files.length > 0) {
       console.log('ID:', this.id); // Verificar el valor del ID
-      console.log('URL:', `${this.adminService.urlbook}/libreria/agregar-imagen/${this.id}`); // Confirmar que la URL es correcta
+      console.log(
+        'URL:',
+        `${this.adminService.urlbook}/libreria/agregar-imagen/${this.id}`,
+      ); // Confirmar que la URL es correcta
       console.log('Archivos a subir:', this.files); // Comprobar los archivos seleccionados
-      this.adminService.agregarImagenesGaleria(this.id, this.files, this.token).subscribe(
-        response => {
-          iziToast.success({
-            title: 'Ok',
-            message: 'Se registraron las imágenes correctamente.',
-            position: 'topRight',
-          });
-          this.init_data();
-          this.files = [];
-          console.log('Respuesta del servidor:', response);
-        },
-        error => {
-          console.error('Error al subir las imágenes:', error);
-          iziToast.error({
-            title: 'Error',
-            message: 'Ocurrió un error al subir las imágenes.',
-            position: 'topRight',
-          });
-        }
-      );
+      this.adminService
+        .agregarImagenesGaleria(this.id, this.files, this.token)
+        .subscribe(
+          (response) => {
+            iziToast.success({
+              title: 'Ok',
+              message: 'Se registraron las imágenes correctamente.',
+              position: 'topRight',
+            });
+            this.init_data();
+            this.files = [];
+            console.log('Respuesta del servidor:', response);
+          },
+          (error) => {
+            console.error('Error al subir las imágenes:', error);
+            iziToast.error({
+              title: 'Error',
+              message: 'Ocurrió un error al subir las imágenes.',
+              position: 'topRight',
+            });
+          },
+        );
     } else {
       iziToast.error({
         title: 'Error',
@@ -131,17 +161,12 @@ export class GaleriaImagenesComponent {
     }
   }
 
-  onReset3() {
-    this.tooltipValidated = false;
-    console.log('Reset... 3');
-  }
-
   onFileChange(event: any) {
     if (event.target.files && event.target.files.length > 0) {
       this.files = Array.from(event.target.files); // Mantén el arreglo de archivos
       const file = this.files[0]; // Toma el primer archivo para validación
-  
-      if (file.size <= 4000000) {
+
+      if (file.size <= 1000000) {
         if (
           file.type === 'image/png' ||
           file.type === 'image/webp' ||
@@ -181,6 +206,5 @@ export class GaleriaImagenesComponent {
       this.imgSelect = 'assets/carpeta.jpg';
     }
     console.log(this.files);
-  }   
-
+  }
 }
