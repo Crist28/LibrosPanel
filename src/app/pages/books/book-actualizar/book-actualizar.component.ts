d88e404a2eb8111838c685ecad9bd0d2c42c808e
Lufer2801 <luisa230313@gmail.com>
2024-12-16 10:14:36 -0500

    Test Prettier en Angular

--- src/app/pages/books/book-actualizar/book-actualizar.component.ts
+++ src/app/pages/books/book-actualizar/book-actualizar.component.ts
@@ -1,7 +1,33 @@
 import { Component, OnInit } from '@angular/core';
-import { ReactiveFormsModule, FormsModule, NgForm ,FormGroup, FormControl} from '@angular/forms';
+import {
+  ReactiveFormsModule,
+  FormsModule,
+  NgForm,
+  FormGroup,
+  FormControl,
+} from '@angular/forms';
 import { DocsExampleComponent } from '@docs-components/public-api';
-import { RowComponent, ColComponent, TextColorDirective, CardComponent, CardHeaderComponent, CardBodyComponent, FormDirective, FormLabelDirective, FormControlDirective, FormFeedbackComponent, InputGroupComponent, InputGroupTextDirective, FormSelectDirective, FormCheckComponent, FormCheckInputDirective, FormCheckLabelDirective, ButtonDirective, ListGroupDirective, ListGroupItemDirective } from '@coreui/angular';
+import {
+  RowComponent,
+  ColComponent,
+  TextColorDirective,
+  CardComponent,
+  CardHeaderComponent,
+  CardBodyComponent,
+  FormDirective,
+  FormLabelDirective,
+  FormControlDirective,
+  FormFeedbackComponent,
+  InputGroupComponent,
+  InputGroupTextDirective,
+  FormSelectDirective,
+  FormCheckComponent,
+  FormCheckInputDirective,
+  FormCheckLabelDirective,
+  ButtonDirective,
+  ListGroupDirective,
+  ListGroupItemDirective,
+} from '@coreui/angular';
 import { DefaultLayoutComponent } from '../../../components/default-layout/default-layout.component';
 import { AdminService } from '../../../services/admin.service';
 import { Router, RouterLink } from '@angular/router';
@@ -40,96 +66,91 @@ declare let iziToast: any;
   styleUrl: './book-actualizar.component.scss',
 })
 export class BookActualizarComponent {
-
   public registerForm: FormGroup;
   public token: string;
-  public file : any = undefined;
-  public imgSelect : any | ArrayBuffer = 'assets/carpeta.jpg';
+  public file: any = undefined;
+  public imgSelect: any | ArrayBuffer = 'assets/carpeta.jpg';
 
-constructor (private adminService: AdminService,private router: Router){
-  const token = this.adminService.getToken();
-  this.token = token !== null ? token : '';
-  const file = this.adminService;
+  constructor(
+    private adminService: AdminService,
+    private router: Router,
+  ) {
+    const token = this.adminService.getToken();
+    this.token = token !== null ? token : '';
+    const file = this.adminService;
 
-  this.registerForm = new FormGroup({
-    titulo: new  FormControl ('',),
-    portada : new  FormControl ('',),
-    contenido: new  FormControl ('',),
-    precio: new  FormControl ('',),
-    stock:new  FormControl ('',),
-    descripcion: new  FormControl ('',),
-    categoria:new  FormControl ('',),
-  });
-  
- 
+    this.registerForm = new FormGroup({
+      titulo: new FormControl(''),
+      portada: new FormControl(''),
+      contenido: new FormControl(''),
+      precio: new FormControl(''),
+      stock: new FormControl(''),
+      descripcion: new FormControl(''),
+      categoria: new FormControl(''),
+    });
+  }
 
-}
-
-onFileChange(event: any) {
-  if (event.target.files.length > 0) {
-    this.file = event.target.files[0];
-    if (this.file.size <= 4000000) {
-      if (
-        this.file.type == 'image/png' ||
-        this.file.type == 'image/webp' ||
-        this.file.type == 'image/jpg' ||
-        this.file.type == 'image/jpeg' ||
-        this.file.type == 'image/gif'
-      ) {
-        const reader = new FileReader();
-        reader.onload = (e) => (this.imgSelect = reader.result);
-        reader.readAsDataURL(this.file);
-        console.log(this.file);
+  onFileChange(event: any) {
+    if (event.target.files.length > 0) {
+      this.file = event.target.files[0];
+      if (this.file.size <= 4000000) {
+        if (
+          this.file.type == 'image/png' ||
+          this.file.type == 'image/webp' ||
+          this.file.type == 'image/jpg' ||
+          this.file.type == 'image/jpeg' ||
+          this.file.type == 'image/gif'
+        ) {
+          const reader = new FileReader();
+          reader.onload = (e) => (this.imgSelect = reader.result);
+          reader.readAsDataURL(this.file);
+          console.log(this.file);
+        } else {
+          iziToast.error({
+            title: 'Error',
+            message: 'El archivo debe ser una imagen',
+            position: 'topRight',
+          });
+          this.imgSelect = 'assets/biblioteca.png';
+          this.file = undefined;
+        }
       } else {
         iziToast.error({
           title: 'Error',
-          message: 'El archivo debe ser una imagen',
+          message: 'La imagen no puede superar los 4MB',
           position: 'topRight',
         });
-        this.imgSelect = 'assets/biblioteca.png';
+        this.imgSelect = 'assets/carpeta.jpg';
         this.file = undefined;
       }
     } else {
       iziToast.error({
         title: 'Error',
-        message: 'La imagen no puede superar los 4MB',
+        message: 'No hay una imagen de envío',
         position: 'topRight',
       });
       this.imgSelect = 'assets/carpeta.jpg';
       this.file = undefined;
     }
-  } else {
-    iziToast.error({
-      title: 'Error',
-      message: 'No hay una imagen de envío',
-      position: 'topRight',
-    });
-    this.imgSelect = 'assets/carpeta.jpg';
-    this.file = undefined;
+    console.log(this.file);
   }
-  console.log(this.file);
-}
-  
- onSubmit(){
-  if(this.registerForm.valid){
-    const formData = this.registerForm.value
-    console.log(formData)
-    this.adminService.CreateBook(formData,this.file,this.token).subscribe(
-      (response) =>{
-        console.log(response)
-        this.router.navigate(['']);
-      },
-      (error) =>{
-        console.log(error)
-        // this.error = error.error.message;
-      }
-      
-    );
 
+  onSubmit() {
+    if (this.registerForm.valid) {
+      const formData = this.registerForm.value;
+      console.log(formData);
+      this.adminService.CreateBook(formData, this.file, this.token).subscribe(
+        (response) => {
+          console.log(response);
+          this.router.navigate(['']);
+        },
+        (error) => {
+          console.log(error);
+          // this.error = error.error.message;
+        },
+      );
+    }
   }
- }
-
-
 
   tooltipValidated = false;
 
@@ -137,8 +158,4 @@ onFileChange(event: any) {
     this.tooltipValidated = false;
     console.log('Reset... 3');
   }
-
 }
-
-
- 
